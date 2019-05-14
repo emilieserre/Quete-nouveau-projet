864e5df52b33ce84b2f7396532b3af911abaad2f
Emilie SERRE <emilieserre@hotmail.com>
2019-05-14 17:15:17 +0200

    quete symfony - récupérer des données stockées avec Doctrine

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,34 +1,24 @@
 <?php
-
-// src/Controller/BlogController.php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
-class BlogController extends AbstractController
-{
-    /**
-     * @Route("/blog", name="blog_index")
-     */
-    public function index()
-    {
-        return $this->render('blog/index.html.twig', [
-'owner' => 'Emilie'
-]);
-}
 /**
-* @Route("/blog/show/{page}",
-*     requirements={"page"="[a-z0-9-]+"},
-*     methods={"GET"},
-*     defaults={"page"= "Article sans titre"},
-*     name="blog_list"
-* )
-*/
-public function show($page)
+ * Show all row from article's entity
+ *
+ * @Route("/", name="index")
+ * @return Response A response instance
+ */
+public function index(): Response
 {
-$page = ucwords(str_replace("-"," ",$page));
-return $this->render('blog/show.html.twig', ['page' => $page]);
-}
+    $articles = $this->getDoctrine()
+        ->getRepository(Article::class)
+        ->findAll();
+
+    if (!$articles) {
+        throw $this->createNotFoundException(
+            'No article found in article\'s table.'
+        );
+    }
+
+    return $this->render(
+        'blog/index.html.twig',
+        ['articles' => $articles]
+    );
 }
