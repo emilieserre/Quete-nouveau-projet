5fc532252085955081e0fd62ff9dd962b91cd774
Emilie SERRE <emilieserre@hotmail.com>
2019-05-20 16:24:10 +0200

    quête symfony - les relations bidirectionnelles avec Doctrine

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -27,10 +27,22 @@ class Article
     private $title;
 
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="articles")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
+
+    public function getCategory(): ?Category
+    {
+        return $this -> category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this -> category = $category;
+
+        return $this;
+    }
 
     public function getId(): ?int
     {
@@ -61,15 +73,5 @@ class Article
         return $this;
     }
 
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
 
-    public function setCategory(?Category $category): self
-    {
-        $this->category = $category;
-
-        return $this;
-    }
 }
