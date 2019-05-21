773605002143ddcd0abfb96c591822f04c3a6856
Emilie SERRE <emilieserre@hotmail.com>
2019-05-21 11:22:01 +0200

    quete symfony - le param converter

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -38,42 +38,15 @@ class BlogController extends AbstractController
             ['articles' => $articles]
         );
     }
+
     /**
-     * Getting a article with a formatted slug for title
-     *
-     * @param string $slug The slugger
-     *
-     * @Route("/{slug<^[a-z0-9-]+$>}",
-     *     defaults={"slug" = null},
-     *     name="blog_show")
-     *  @return Response A response instance
+     * @Route("/article/{id}", name="article_show")
      */
-    public function show(?string $slug) : Response
+    public function show(Article $article): Response
     {
-        if (!$slug) {
-            throw $this
-                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
-        }
-        $slug = preg_replace(
-            '/-/',
-            ' ', ucwords(trim(strip_tags($slug)), "-")
-        );
-        $article = $this->getDoctrine()
-            ->getRepository(Article::class)
-            ->findOneBy(['title' => mb_strtolower($slug)]);
-        if (!$article) {
-            throw $this->createNotFoundException(
-                'No article with '.$slug.' title, found in article\'s table.'
-            );
-        }
-        return $this->render(
-            'blog/show.html.twig',
-            [
-                'article' => $article,
-                'slug' => $slug,
-            ]
-        );
+        return $this->render('article.html.twig', ['article'=>$article]);
     }
+
     /**
      * @param string $categoryName
      * @Route("/category/{categoryName}", name="show_category")
